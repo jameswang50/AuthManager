3ccdd37ca5411271240b639dfc4503abed98081a
superpro1226 <javonwoods252@gmail.com>
2024-04-18 18:03:34 -0700

    added balance domain

--- src/main/java/com/mycompany/myapp/domain/Balance.java
+++ src/main/java/com/mycompany/myapp/domain/Balance.java
@@ -3,7 +3,6 @@ package com.mycompany.myapp.domain;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import java.io.Serializable;
 
 public class Balance implements Serializable {
@@ -14,4 +13,11 @@ public class Balance implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
