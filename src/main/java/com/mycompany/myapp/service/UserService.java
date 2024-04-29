6fff84c519bb1d44cca4f1cc9e8ac24637c9e7bf
superpro1226 <javonwoods252@gmail.com>
2024-04-30 01:50:08 +0200

    updated user service

--- src/main/java/com/mycompany/myapp/service/UserService.java
+++ src/main/java/com/mycompany/myapp/service/UserService.java
@@ -122,7 +122,6 @@ public class UserService {
         }
         newUser.setImageUrl(userDTO.getImageUrl());
         newUser.setLangKey(userDTO.getLangKey());
-        // new user is not active
         newUser.setActivated(false);
         // new user gets registration key
         newUser.setActivationKey(RandomUtil.generateActivationKey());
